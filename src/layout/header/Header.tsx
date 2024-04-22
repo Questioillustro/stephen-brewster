81f256700a62a8033b330cbd69c1785250597f6d
Stephen Brewster <questioillustro@gmail.com>
2024-04-22 12:06:47 -0400

    linkedin, code tiles

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -4,7 +4,8 @@ import { css } from '@emotion/react';
 import Typography from '@mui/material/Typography';
 import React from 'react';
 import { HeaderStyle } from './Header.style';
-import Growing from '../../components/animation/growing/Growing';
+import { Link } from '@mui/material';
+import { LinkedIn } from '@mui/icons-material';
 
 function Header() {
   return (
@@ -15,6 +16,10 @@ function Header() {
       <Typography variant={'subtitle1'} color={'tertiary'}>
         Software Engineer
       </Typography>
+
+      <Link href={'https://www.linkedin.com/in/stephen-brewster-5b3180166/'} target={'_blank'}>
+        <LinkedIn />
+      </Link>
     </div>
   );
 }
---
+++ src/resume/code/Code.hooks.ts
@@ -0,0 +1,27 @@
+﻿export interface ICodeItem {
+  href: string;
+  linkText: string;
+  description: string;
+}
+
+export const useCodeHooks = (): ICodeItem[] => {
+  return [
+    {
+      href: 'https://github.com/Questioillustro/stephen-brewster',
+      linkText: 'This Site',
+      description:
+        'This site is written in React with Typescript and deployed through an Azure pipeline',
+    },
+    {
+      href: 'https://github.com/Questioillustro/portfolio',
+      linkText: 'College Coursework',
+      description:
+        'A collection of software projects completed as part of my coursework at RIT and MCC',
+    },
+    {
+      href: 'https://www.codewars.com/users/Questioillustro/stats',
+      linkText: 'Code Wars Profile',
+      description: 'I enjoy practicing my skills with the occasional Kata on Code Wars',
+    },
+  ];
+};
