f6317690ee7be098faf4ea6a9fea5d1ebb92f3db
Stephen Brewster <questioillustro@gmail.com>
2025-03-20 15:12:51 -0400

    test

--- src/apps/about/About.tsx
+++ src/apps/about/About.tsx
@@ -8,10 +8,10 @@ import QuoteCarousel from '@/apps/about/quotes/QuoteCarousel';
 function About() {
   return (
     <div>
+      Hi, I'm Stephen Brewster's website. Take a look around.
       <Box>
         <QuoteCarousel />
       </Box>
-
       <Sliding
         direction={'left'}
         child={<ResumeCard title={'Interests'} content={<Interests />} />}
