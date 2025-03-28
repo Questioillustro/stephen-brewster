bb2078e99c24dbffae45281ec9303a24e2bb2eca
Stephen Brewster <questioillustro@gmail.com>
2025-03-28 17:08:26 -0400

    buttons

--- src/apps/cyoa/components/activestory/inputs/VentureButtons.tsx
+++ src/apps/cyoa/components/activestory/inputs/VentureButtons.tsx
@@ -6,13 +6,18 @@ import { ActiveStoryViews } from '@/apps/cyoa/components/activestory/ActiveStory
 const ButtonContainer = styled('div')({
   display: 'flex',
   width: '100%',
-  gap: '1.25rem', // Optional: adds space between buttons
+  gap: '1.25rem',
+  maxWidth: '775px',
+  margin: '1rem',
   flexDirection: 'column',
 });
 
 const StyledButton = styled(Button)({
-  flex: 1, // Each button takes approximately half the space
-  margin: '2rem',
+  flex: 1,
+  maxHeight: '250px',
+  backgroundSize: 'contain',
+  backgroundPosition: 'center',
+  backgroundRepeat: 'no-repeat',
 });
 
 export interface IVentureButtonsProps {
@@ -23,12 +28,38 @@ const VentureButtons: React.FC<IVentureButtonsProps> = (props: IVentureButtonsPr
   const { setActiveView } = props;
   return (
     <ButtonContainer>
-      <StyledButton variant='contained' onClick={() => setActiveView('build')}>
-        Build-A-Venture
-      </StyledButton>
-      <StyledButton variant='contained' onClick={() => setActiveView('revisit')}>
-        Revisit-A-Venture
-      </StyledButton>
+      <StyledButton
+        variant='contained'
+        onClick={() => setActiveView('build')}
+        sx={{
+          backgroundImage:
+            "url('https://azurefunctionsapp2022060.blob.core.windows.net/cyoa/build-a-venture_button.jpg')",
+          backgroundColor: 'black',
+          borderRadius: '12px',
+          boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+          '&:hover': {
+            backgroundColor: '#FFFFFF',
+            transform: 'scale(1.05)',
+            boxShadow: '0 5px 15px rgba(0,0,0,0.3)',
+          },
+        }}
+      ></StyledButton>
+      <StyledButton
+        variant='contained'
+        onClick={() => setActiveView('revisit')}
+        sx={{
+          backgroundImage:
+            "url('https://azurefunctionsapp2022060.blob.core.windows.net/cyoa/revisit-a-venture_button.jpg')",
+          backgroundColor: 'black',
+          borderRadius: '12px',
+          boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+          '&:hover': {
+            backgroundColor: '#FFFFFF',
+            transform: 'scale(1.05)',
+            boxShadow: '0 5px 15px rgba(0,0,0,0.3)',
+          },
+        }}
+      ></StyledButton>
     </ButtonContainer>
   );
 };
--- src/apps/cyoa/components/activestory/storypresentation/RevisitControls.tsx
+++ src/apps/cyoa/components/activestory/storypresentation/RevisitControls.tsx
@@ -18,47 +18,44 @@ const RevisitControls: React.FC<IRevisitControlsProps> = (props: IRevisitControl
         display: 'flex',
         width: '100%',
         flexDirection: 'column',
-        rowGap: '20px',
+        textAlign: 'center',
+        rowGap: '1.25rem',
       }}
-      sx={{ p: 2 }}
     >
-      <Paper square elevation={1} sx={{ p: 2 }}>
-        <Typography variant={'h6'} color={'primary'}>
-          Revisit-A-Story
-        </Typography>
+      <Typography variant={'h6'} color={'secondary'} sx={{ pt: 1 }}>
+        Revisit previously generated stories
+      </Typography>
 
-        <Typography variant={'body1'} color={'secondary'}>
-          Revisit a previously generated story
-        </Typography>
+      <Paper
+        sx={{
+          p: 1,
+          flexDirection: 'row',
+          display: 'flex',
+          justifyContent: 'space-around',
+        }}
+      >
+        <Button
+          startIcon={<ArrowBack />}
+          onClick={previousVersion}
+          disabled={currentVersionNumber === 0}
+          variant={'outlined'}
+        >
+          Previous Version
+        </Button>
 
-        <Grid container spacing={2} sx={{ p: 2 }}>
-          <Grid item>
-            <Button
-              startIcon={<ArrowBack />}
-              onClick={previousVersion}
-              disabled={currentVersionNumber === 0}
-              variant={'outlined'}
-            >
-              Previous Version
-            </Button>
-          </Grid>
-
-          <Grid item>
-            <Button
-              endIcon={<ArrowForward />}
-              onClick={nextVersion}
-              disabled={currentVersionNumber + 1 === total}
-              variant={'outlined'}
-            >
-              Next Version
-            </Button>
-          </Grid>
-        </Grid>
-
-        <Typography variant={'body2'} color={'secondary'}>
-          Currently displaying version: {currentVersionNumber + 1} / {total}
-        </Typography>
+        <Button
+          endIcon={<ArrowForward />}
+          onClick={nextVersion}
+          disabled={currentVersionNumber + 1 === total}
+          variant={'outlined'}
+        >
+          Next Version
+        </Button>
       </Paper>
+
+      {/*<Typography variant={'body2'} color={'secondary'}>*/}
+      {/*  Currently displaying version: {currentVersionNumber + 1} / {total}*/}
+      {/*</Typography>*/}
     </Paper>
   );
 };
