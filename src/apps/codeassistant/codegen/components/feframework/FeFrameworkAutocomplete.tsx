720bf44cb15820cddbb31efb48331abbe3139dfa
Stephen Brewster <questioillustro@gmail.com>
2025-05-03 22:24:59 -0400

    allow any framework

--- src/apps/codeassistant/codegen/components/feframework/FeFrameworkAutocomplete.tsx
+++ src/apps/codeassistant/codegen/components/feframework/FeFrameworkAutocomplete.tsx
@@ -2,14 +2,28 @@
 import { FrontEndFrameworkOptionsLabels } from '@/apps/codeassistant/codegen/data/AllOptions';
 import { Autocomplete, TextField } from '@mui/material';
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 export const FeFrameworkAutocomplete = () => {
   const context = useCodegenContext();
+  const [inputValue, setInputValue] = useState(context.codeGen.request.framework ?? '');
+
+  useEffect(() => {
+    setInputValue(context.codeGen.request.framework ?? '');
+  }, [context.codeGen.request.framework]);
 
   const handleFrameworkChange = (event: React.SyntheticEvent, value: string | null) => {
+    const newValue = value ?? '';
+    setInputValue(newValue);
     context.updateCodeGen({
       request: { ...context.codeGen.request, framework: value ?? '' },
+    });
+  };
+
+  const handleInputChange = (event: React.SyntheticEvent, newInputValue: string) => {
+    setInputValue(newInputValue);
+    context.updateCodeGen({
+      request: { ...context.codeGen.request, framework: newInputValue },
     });
   };
 
@@ -18,6 +32,7 @@ export const FeFrameworkAutocomplete = () => {
       options={Object.values(FrontEndFrameworkOptionsLabels)}
       value={context.codeGen.request.framework ?? ''}
       onChange={handleFrameworkChange}
+      onInputChange={handleInputChange}
       renderInput={(params) => <TextField {...params} label='FE Framework' variant='outlined' />}
       sx={{ width: '100%' }}
     />
